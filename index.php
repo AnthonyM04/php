5c08c9343726c9dadfad964763a6b52033fc947e
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-09-09 14:11:14 -0400

    variables, special quotes

--- index.php
+++ index.php
@@ -3,11 +3,34 @@
 
 ?>
 
-<?
+<?php
   print 'Print is functionally the same as echo.';
 /* You can 
   add a comment */
+?>
 
-echo <<<HERE
-<p>There's also something called heredoc that allows you to print multiple lines without stopping.
-It will look for a defined identifier. </p>
+<!-- what are variables?  -->
+
+<!-- variables must start with a $ followed by a letter or an underscore, then a alpha numberic character string! -->
+
+<!-- $variables = 'containers for storing information that will be referenced later! such as names, numbers, and other data.'; -->
+
+<?php
+  $age = 36;
+  echo "Single quotes\nJoe is $age years old!\nDouble quotes\n";
+
+  echo 'Joe is $age years old!';  
+?>
+
+<!-- how to use variables in single quotes;  -->
+
+<?php
+  $age = 36;
+  echo 'Brenda is'. $age .'years old.'; // concatenation!
+?>
+
+<!-- how to deal with quotes and other special characters. -->
+<?php
+  echo "Joe's nickname in high school was \"Crazy Joe.\"";
+  // the back lashes tell the computer to ignore what is between and to not treat those characters as code!
+?>
