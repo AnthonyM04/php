230f4f1006b5a62f52b5b32a7ddaaa67e16e7977
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-09-10 16:23:09 -0400

    echo number until it reaches 10

---
+++ loops.php
@@ -0,0 +1,14 @@
+<?php
+  $i = 0;
+  while( $i <= 10 ) {
+    echo "$i <br/>";
+    $i++;
+  }
+
+  for( $i = 0; $i <= 10; $i++ ) {
+    echo "$i <br/";
+  }
+
+  for( $i = 0; $i <= 10; $i++ ) {
+    echo "$i <br/>";
+  }
